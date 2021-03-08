de7d461fb6b204cbdaefbc8b2df676e8b5d27b74
Marc <marc.castillo@berkeley.edu>
2021-03-08 00:09:16 -0500

    wrote another funciton

---
+++ R/oldest_player.R
@@ -0,0 +1,13 @@
+#' Oldest player
+#'
+#' Returns oldest player during a given year. If multiple oldest players, returns the first one alphabetically by first name.
+#' @param year Year of interest
+#' @keywords oldest player
+#' @export
+#' @examples
+#' oldest_player(2015)
+#' "Andre Miller"
+oldest_player <- function(year){
+  filtered = dplyr::filter(nba, Year==year) %>% dplyr::arrange(desc(Age), Player) %>% dplyr::select(Player)
+  return(filtered[1, ])
+}
