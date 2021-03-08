1df6e65a08aa9050acfcedbe19bb27a9919fa9aa
Marc Castillo <marc.castillo@berkeley.edu>
2021-03-07 23:46:04 -0500

    initial commit

---
+++ R/nba.R
@@ -0,0 +1,4 @@
+#' Seasonal NBA stats
+#' @format A data frame with 24691 observations and 52 variables
+#' @source \url{https://www.kaggle.com/drgilermo/nba-players-stats/data}
+"nba"
---
+++ R/player_points.R
@@ -0,0 +1,14 @@
+#' Player points
+#'
+#' This function returns the points scored by a given player (Michael Jordan by default) in a given year.
+#' @param year Year of interest
+#' @param player default=Michael Jordan
+#' @keywords points
+#' @export
+#' @examples
+#' player_points(2015)
+#' whatever the points are
+player_points <- function(year, player="Michael Jordan*"){
+  filtered = dplyr::filter(nba, Year==year, Player==player)
+  return(filtered["PTS"])
+}
